3ac4b89ced46527e74277a7fec08c6df1df42a37
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-08 13:17:29 +0600

    conditional savings

---
+++ module_2/tk.py
@@ -0,0 +1,10 @@
+taka = 50
+
+if taka > 500:
+    print("I will buy a laptop")
+elif taka > 400:
+    print("I will buy a tablet")
+elif taka > 300:
+    print("I will buy a mobile")
+else:
+    print("Save money")
