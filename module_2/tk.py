13de55189508e9e52e9e3dd6cebebb8d8bc2b382
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-08 13:44:27 +0600

    statement continue

--- module_2/tk.py
+++ module_2/tk.py
@@ -12,8 +12,18 @@
 # for i in range(11):
 #     print(i , "Hello World")
 
-count = 1
+# count = 1
 
-while count <= 10:
-    print(count, "Hello World")
-    count = count + 1
+# while count <= 10:
+#     print(count, "Hello World")
+#     count = count + 1
+
+sum = 0
+for i in range(1,11):
+    print (i, "is processing")
+
+    if i % 2 == 0: 
+       continue
+
+    sum = sum + i
+    print(i , "is added to the sum")
