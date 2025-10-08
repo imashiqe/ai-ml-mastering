9dd0f5863972b6ddf83f925b8118a07293c3188a
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-08 13:27:14 +0600

    for loop

--- module_2/tk.py
+++ module_2/tk.py
@@ -1,10 +1,13 @@
-taka = 50
+# taka = 50
 
-if taka > 500:
-    print("I will buy a laptop")
-elif taka > 400:
-    print("I will buy a tablet")
-elif taka > 300:
-    print("I will buy a mobile")
-else:
-    print("Save money")
+# if taka > 500:
+#     print("I will buy a laptop")
+# elif taka > 400:
+#     print("I will buy a tablet")
+# elif taka > 300:
+#     print("I will buy a mobile")
+# else:
+#     print("Save money")
+
+for i in range(11):
+    print(i , "Hello World")
