277720f3594899b3e72d12b667bacc118aa33074
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-08 11:09:03 +0600

    Artificial Intelligance Machine Learning

---
+++ module_01/slicing.py
@@ -0,0 +1,21 @@
+nums = [10, 20, 30, 40, 50, 60]
+print(nums[1:4])   # middle slice
+print(nums[-3:])   # last 3
+print(nums[::2])   # step slice
+print(nums[::-1])  # reverse
+print(nums[:-3])
+
+
+phoneBook = {
+     'Alice': '703-493-1834',
+     'Bob': '857-384-1234',
+     'Charlie': '503-493-1834'
+}
+
+
+# print(phoneBook['Alice'])
+
+
+s = {1,1,2,3,4,4,5,6,7,8,9}
+
+print(s)
